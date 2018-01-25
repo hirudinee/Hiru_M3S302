0aef9dc3d65119a18cc11cd217cdb2a516ab43f8
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-25 11:21:45 +0530

    sns

--- Hiru_M3S302/lambda.js
+++ Hiru_M3S302/lambda.js
@@ -1,30 +1,20 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 let connectionManager = require('./ConnectionManager');
 let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
-
-	// Replace the query with the actual query
-	// You can pass the existing connection to this function.
-	// A new connection will be created if it's not present as the third param 
-	// You must always end the DB connection after it's used
-	rds.query({
-		instanceIdentifier: 'test3',
-		query: 'test 123',
-		inserts: []
-	}, function (error, results, connection) {
-		if (error) {
-			console.log("Error occurred");
-			throw error;
-		} else {
-			console.log("Success")
-			console.log(results);
-		}
-
-		connection.end();
-	});
+	sns.getTopicAttributes({
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:upulie_topic_jan17'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
 
 
 	callback(null, 'Successfully executed');
