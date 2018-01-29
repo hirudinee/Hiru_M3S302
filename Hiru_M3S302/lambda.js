19abf77e74474ef33d5c65cf06b86af2d3b652a3
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 15:28:56 +0530

    new bucket

--- Hiru_M3S302/lambda.js
+++ Hiru_M3S302/lambda.js
@@ -1,21 +1,6 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-let connectionManager = require('./ConnectionManager');
-let SL = require('@slappforge/slappforge-sdk');
-const rds = new SL.AWS.RDS(connectionManager);
-const s3 = new AWS.S3();
-exports.handler = function (event, context, callback) {
-
-	sns.getTopicAttributes({
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:upulie_topic_jan17'
-	}).promise()
-		.then(data => {
-			// your code goes here
-		})
-		.catch(err => {
-			// error handling goes here
-		});
+exports.handler = function(event, context, callback) {
 
 
-	callback(null, 'Successfully executed');
+	callback(null,'Successfully executed');
 }
